361057052611d4d48fe4283b175ab02a81c51aef
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2019-10-20 21:16:43 -0400

    Feature - generate package dependencies - generate package dependency graph with -s 2 or --step 2   - Support for relative import not guaranteed - README.md -> description

--- twelve_step/arguments.py
+++ twelve_step/arguments.py
@@ -10,6 +10,13 @@ def get_arguments() -> Arguments:
         "project_path", help="Path to the project to generate dependency graph for."
     )
     parser.add_argument(
+        "-s",
+        "--step",
+        default=1,
+        help="Choose which step of twelve-step you want to execute",
+    )
+    parser.add_argument(
+        "-e",
         "--exclude-packages",
         nargs="*",
         default=[],
@@ -22,12 +29,16 @@ def get_arguments() -> Arguments:
     )
     arguments = parser.parse_args()
     return Arguments(
-        arguments.project_path, arguments.exclude_packages, arguments.output_file
+        arguments.project_path,
+        arguments.step,
+        arguments.exclude_packages,
+        arguments.output_file,
     )
 
 
 @dataclass
 class Arguments:
     project_path: str
+    step: int
     exclude_packages: List[str]
     output_file: Optional[str]
